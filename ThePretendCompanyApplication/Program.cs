77cf94a87d559de397c314111bdecbf496ca9ff7
desha <desha@LAPTOP-0FILGKU0>
2023-10-13 11:28:18 +0100

    Add project files.

---
+++ ThePretendCompanyApplication/Program.cs
@@ -0,0 +1,146 @@
+﻿using TCPData;
+using TCPExtensions;
+
+//Console.WriteLine("Employees ");
+//List<Employee> employeeList = Data.GetEmployees(); 
+////Custom method declared
+//var filteredEmployee = employeeList.Filter(emp =>emp.IsManager == true); 
+//var filteredEmployeeByLINQ = employeeList.Where(emp =>emp.IsManager == true);
+
+
+//foreach (var employee in filteredEmployee)
+//{
+//    Console.WriteLine($"First Name: {employee.FirstName}");
+//    Console.WriteLine($"Last Name : {employee.LastName}");
+//    Console.WriteLine($"Annual Salary : {employee.AnnualSalary}");
+//    Console.WriteLine($"Manager : {employee.IsManager}");
+//    Console.WriteLine("");
+//}
+
+//Console.ReadKey();
+//Console.WriteLine("Department");
+//List<Department> departmentList = Data.GetDepartments();
+//var filterDepartments = departmentList.Filter(dep => dep.ShortName == "HR" || dep.ShortName == "TE");
+//foreach(var department in filterDepartments)
+//{
+//    Console.WriteLine($"Id : {department.Id}"); 
+//    Console.WriteLine($"short Name : {department.ShortName}");
+//    Console.WriteLine($"Long Name : {department.LongName}");
+//    Console.WriteLine("");
+//}
+
+////Linq query Results
+////Li0st<Employee> employeeList = Data.GetEmployees();
+////List<Department> departmentList = Data.GetDepartments();
+//Console.WriteLine("LINQ QUERIES");
+//var resultList = from employee in employeeList
+//                 join department in departmentList
+//                 on employee.DepartmentId equals department.Id
+//                 select new
+//                 {
+//                     FirstName = employee.FirstName,
+//                     LastName = employee.LastName,
+//                     AnnualSalary = employee.AnnualSalary,
+//                     Manager = employee.IsManager,
+//                     Department = department.LongName
+//                 };
+//Console.ReadKey();
+
+//foreach( var result in resultList)
+//{
+//    Console.WriteLine($"{result.FirstName} works at {result.Department}");
+//}
+
+////above query will be like this
+////SELECT
+///// e.FirstName ,e.LastNamee,e.AnnualSalary ,e.IsManager , d.LongName
+////FROM
+////  Employee e 
+////INNER JOIN
+//// department d 
+////ON 
+////  e.DEpartmentId  = d.Id;
+
+
+//Console.WriteLine("LINQ METHODS");
+//var averageSalary = resultList.Average(emp => emp.AnnualSalary);
+//Console.WriteLine($"average slary of  an employee is about {averageSalary}");
+
+//var highestSalary = resultList.Max(emp => emp.AnnualSalary);
+//var highestSalaryHolder = resultList.SingleOrDefault(emp => emp.AnnualSalary == highestSalary);
+
+//Console.WriteLine($"{highestSalaryHolder.FirstName} {highestSalaryHolder.LastName} earnshigest salary which is  {highestSalary}");
+
+//var lowestSal = resultList.Min(emp => emp.AnnualSalary);
+//var lowestSalaryHolder = resultList.SingleOrDefault(emp=>emp.AnnualSalary == lowestSal).FirstName;
+//Console.WriteLine($"{lowestSalaryHolder} earns the minimum salary ({lowestSal}) amounng other mates");
+
+
+List<Department> departmentList = Data.GetDepartments();
+List<Employee> employeelist = Data.GetEmployees();
+
+Console.WriteLine("Linq -Method Syntax");
+
+var employeeRecData = employeelist
+    .Select(e =>
+        new {
+            FullName = e.FirstName + " " + e.LastName,
+            Salary = e.AnnualSalary
+
+        })
+    .Where(e => e.Salary < 50000)
+    ;
+
+
+foreach (var employee in employeeRecData)
+{
+    Console.WriteLine($"{employee.FullName,15} earns {employee.Salary,10}");
+}
+Console.ReadKey();
+
+Console.WriteLine("Linq Query syntax");
+var EmployeeDatabyQuery = from emp in employeelist
+                          select new
+                          {
+                              FullName = emp.FirstName + " " + emp.LastName,
+                              Salary = emp.AnnualSalary
+                          };
+
+foreach(var emp in EmployeeDatabyQuery)
+{
+    Console.WriteLine(emp.FullName + " ->" + emp.Salary);
+}
+
+Console.WriteLine("employees that have graeater ssalaray than 50  k fetched by LINQqueries");
+
+var empAbvFifty = from emp in employeelist
+                  where emp.AnnualSalary > 50000
+                  select new
+                  {
+                      FullName = emp.FirstName + "" + emp.LastName,
+                      Salary = emp.AnnualSalary
+                  };
+
+//due to dffereed execution we can add this record and expedcrt the record to be in the followiing  
+// dataset 
+
+
+employeelist.Add(
+    new Employee
+    {  
+        FirstName  = "Deshan",
+        LastName = "Maduranga",
+        AnnualSalary = 100000,
+        IsManager = false,
+    }
+   );
+foreach(var emp in empAbvFifty)
+{
+    Console.WriteLine(emp.FullName + " ->" + emp.Salary);
+}
+
+
+
+//In LINQ, queries have two different behaviors of execution: immediate and deferred.
+//defered execution happen when you iterate simply,with in the for loop 
+
