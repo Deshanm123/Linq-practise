53c7cfecdb3f6e318ae7b656716b7fac51c5c296
Deshanm123 <deshanm123@gmail.com>
2023-10-16 21:36:46 +0100

    Revert "Revert "concatanation -linq""
    This reverts commit d6cb73065f6db16db075f59f1c4b116f234e6c1f.

--- ThePretendCompanyApplication/Program.cs
+++ ThePretendCompanyApplication/Program.cs
@@ -6,54 +6,35 @@ using TCPData;
 using TCPExtensions;
 
 
+//concatanattion f list
 
-
-//SequenceEqual Evaluation as follows
-////true if the two source sequences are of equal length and their corresponding elements are equal according to the default equality comparer for their type
-//; otherwise, false.
 List<int> oddIntegersList = new List<int>() {1,3,5,7,9 };
 
-List<int> oddIntegersListClone = new List<int>() {1,3,5,7,9 };
-var result = oddIntegersList.SequenceEqual(oddIntegersListClone);
-Console.WriteLine("Is oddIntegers List is same Seqeunce as oddIntegersListClone ?" + result);
-
-//var reverse = oddIntegersListClone.Reverse();
-//var result2 = oddIntegersList.SequenceEqual();
-oddIntegersListClone.Reverse();
-var newResult = oddIntegersList.SequenceEqual(oddIntegersListClone);
-Console.WriteLine("Is oddIntegers List is same Seqeunce as oddIntegersListClone but its rearranged in order ?" + newResult);
-
-//comparing objects using sequence equal
+List<int> evenIntegersList = new List<int>() {2,4,6,8, };
 
 
-
-var employeeList = Data.GetEmployees();
-var employeeListClone = Data.GetEmployees();
-
-
-Console.WriteLine("Comparing objects using SequenceEqual");
-
-var employeeCompareResult= employeeList.SequenceEqual(employeeListClone);
-Console.WriteLine(" employeeList and employeeListClone are two instancces of employee object but are they gonna equal  ?" + newResult);
-//NO 
-
-var empCompareByIEqualityComparer = employeeList.SequenceEqual(employeeListClone ,new EmployeeComparer());
-Console.WriteLine(" \n employeeList and employeeListClone are two instancces of employee object but are they gonna equal  when using IequalityComparer ?" + empCompareByIEqualityComparer);
-
-
-public class EmployeeComparer : IEqualityComparer<Employee>
+var numberList = oddIntegersList.Concat(evenIntegersList);
+foreach (var number in numberList)
 {
-    public bool Equals(Employee? x, Employee? y)
-    {
-        if(x.Id == y.Id  && x.FirstName.ToLower() == y.FirstName.ToLower()  && x.LastName == y.LastName)
-        {
-            return true;
-        }
-        return false;
-    }
+    Console.WriteLine(number);
+}
 
-    public int GetHashCode([DisallowNull] Employee obj)
-    {
-        return obj.Id.GetHashCode();
-    }
+
+
+//Employee 
+
+ List<Employee> employees = Data.GetEmployees();
+
+List<Employee> newEmployeesList = new List<Employee>()
+{
+    new Employee{Id = 90 , FirstName="Jack", LastName="Sparrrow"},
+    new Employee{Id= 91,FirstName ="Davy",LastName="Johnson"}
+};
+
+employees.Concat(newEmployeesList);
+
+
+foreach (var employee in employees)
+{
+    Console.WriteLine(employee.FirstName +" - " + employee.LastName);
 }
